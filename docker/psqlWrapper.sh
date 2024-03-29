971b12731ada4ac5f949790d7dd18d7b5e02bccf
luka19517 <luka.djorovic3010@gmail.com>
2024-03-29 23:10:03 +0100

    user friendliness message

--- docker/psqlWrapper.sh
+++ docker/psqlWrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Docker psql wrapper'
-
+echo '----Docker psql wrapper----'
+echo 'Enter psql command'
 read command
 docker exec postgres psql -U postgres -d postgresdb -c "$command"
