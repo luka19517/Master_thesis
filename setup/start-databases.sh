6e6491e9f910b220fbc331fdab22120031d48d74
luka19517 <luka.djorovic3010@gmail.com>
2024-04-18 20:42:08 +0200

    start setup hbase returned

--- setup/start-databases.sh
+++ setup/start-databases.sh
@@ -2,6 +2,8 @@
 echo 'PREPARING POSTGRES ENVIRONMENG...';
 docker-compose -f docker-compose.yml up --build -d;
 echo 'PREPARING HBASE ENVIRONMENT...';
+#$HBASE_HOME/bin/start-hbase.sh;
+#java -jar ./hbase_setup_model.jar;
 
 
 
